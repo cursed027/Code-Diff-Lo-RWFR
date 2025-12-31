31a0352dafb2fce0a80d3c20eb12d72a1d9a84dc
Cursed <milanpvt027@gmail.com>
2025-12-31 11:48:53 +0530

    Update version.py

--- diffbir/utils/codeformer/basicsr/version.py
+++ diffbir/utils/codeformer/basicsr/version.py
@@ -1,16 +1,3 @@
-# basicsr/version.py
+__version__ = "1.3.2"
+__gitsha__ = "vendored"
 
-import os
-
-__gitsha__ = "unknown"
-
-def _read_version():
-    root = os.path.dirname(__file__)
-    version_file = os.path.join(root, "VERSION")
-    try:
-        with open(version_file, "r") as f:
-            return f.read().strip()
-    except Exception:
-        return "0.0.0"
-
-__version__ = _read_version()
